8ab2e904be5f641228f37c7b2bf0da078788204a
sixfeetbruh <109311392+sixfeetbruh@users.noreply.github.com>
2023-04-03 21:15:47 +0000

    Deploying to gh-pages from @ sixfeetbruh/goit-react-hw-02-feedback@5ddbcfb18998d1973a554808a7f323c5c6028203 🚀

--- src/components/FeedbackComponent/Notification/Notification.styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-export const NotificationTitle = styled.h2`
-  text-align: center;
-`;
--- src/components/Section/Section.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Title = styled.h2`
-  margin-bottom: 20px;
-  text-align: center;
-`;
